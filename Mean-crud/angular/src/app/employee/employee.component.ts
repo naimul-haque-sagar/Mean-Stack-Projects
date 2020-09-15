097e951ea11fff433a19d9daed9407235372d22d
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-09-16 00:01:37 +0600

    angular functionality added

---
+++ Mean-crud/angular/src/app/employee/employee.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Employee } from '../model/employee-model';
+import {EmployeeService} from '../service/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(public employeeService:EmployeeService) { }
+
+  ngOnInit() {
+    this.resetForm();
+    this.refreshEmployeeList();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form)
+      form.reset();
+    this.employeeService.selectedEmployee = {
+      _id: "",
+      name: "",
+      position: "",
+      office: "",
+      salary: null
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('on submit')
+    if (form.value._id == "") {
+      this.employeeService.postEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+      });
+    }
+  }
+
+  refreshEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe((res) => {
+      this.employeeService.employees = res as Employee[];
+    });
+  }
+}
--- Mean-crud/nodejs/index.js
+++ Mean-crud/nodejs/index.js
@@ -1,11 +1,14 @@
 const dbConnection=require('./db-connection')
 const employeeController=require('./controllers/employeeController.js')
 const express=require('express')
+var cors=require('cors')
 
 const port=process.env.port||8000
 
 var app=express()
 app.use(express.json())
+app.use(cors({ origin :'http://localhost:4200'}))
+
 app.listen(port,()=>{
     console.log(`server started at ${port}`)
 })
